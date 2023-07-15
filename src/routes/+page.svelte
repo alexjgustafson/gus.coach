2cf34b805f9d06d7fefa5e2a482b17a46a486fbe
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-07-14 23:43:57 -0400

    Add coach-in-training content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,20 @@
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">Gus's Barbell Club</h1>
       <h2 class="text-3xl py-4">Train the Starting Strength Method</h2>
-      <p class="py-2">Alex "Gus" Gustafson, Coach</p>
-      <p class="py-2">Louisville, KY</p>
+      <p class="py-2">Alex "Gus" Gustafson, Coach-in-Training</p>
+      <p class="py-2">Starting Strength in Louisville, KY</p>
       <p class="text-xl py-8">Call or text to start: (502) 472-3042</p>
     </div>
+  </div>
+</div>
+<div class="flex flex-col w-full border-opacity-50 text-center py-12">
+  <div class="px-2">
+    <h2 class="text-2xl md:text-3xl py-4 font-bold">Coach-in-Training Special</h2>
+    <p >Gus is pursuing the Starting Strengh Coach (SSC) credential. As thanks for being one of his initial clients, all sessions are currently "pay what you want."</p>
+    <p class="flex flex-col text-center py-6">
+      <span class="text-3xl font-bold">$20</span>
+      <span class="text-xs">recommended per 1-hour session</span>
+    </p>
   </div>
 </div>
 <footer class="footer p-10 bg-neutral text-neutral-content">
