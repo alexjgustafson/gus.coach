c037617712996b499fb44bf0db9e2cc33c8f1f40
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-07-14 23:06:32 -0400

    Improve style for splash page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
-<h1>Gus's Barbell Club</h1>
-
-<h2>Train the Starting Strength Method</h2>
-<p>Alex "Gus" Gustafson, Coach</p>
-<p>Louisville, KY</p>
-<p>Call or text to start: (502) 472-3042</p>
-
-
-<blockquote>
-  Strong people are harder to kill than weaker people, and more useful in general.
-  <cite>- Mark Rippetoe</cite>
-</blockquote>
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Gus's Barbell Club</h1>
+      <h2 class="text-3xl py-4">Train the Starting Strength Method</h2>
+      <p class="py-2">Alex "Gus" Gustafson, Coach</p>
+      <p class="py-2">Louisville, KY</p>
+      <p class="text-xl py-8">Call or text to start: (502) 472-3042</p>
+    </div>
+  </div>
+</div>
+<footer class="footer p-10 bg-neutral text-neutral-content">
+  <blockquote>
+    Strong people are harder to kill than weak people, and more useful in general.
+    <cite>- Mark Rippetoe</cite>
+  </blockquote>
+</footer>
