616da39fb80bd36479aadacff4b2a95c93e32f78
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-07-15 10:37:17 -0400

    new section with image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,29 @@
     </div>
   </div>
 </div>
-<div class="flex flex-col w-full border-opacity-50 text-center py-12">
-  <div class="px-2">
+<div class="w-full border-opacity-50 py-12">
+  <div class="container mx-auto px-2">
+    <h2 class="text-2xl md:text-3xl py-4 mb-8 font-bold text-center">Starting Strength: The Key to Optimal Results</h2>
+    <div class="flex flex-col lg:flex-row px-2 lg:px-24 xl:px-48">
+      <div class="flex-1 lg:pr-6 pb-12">
+        <p>As a method designed to promote rapid strength gains and overall fitness improvement, Starting Strength will help you achieve the best results possible. Whether new to strength training or an experienced lifter, this proven method can help you break through plateaus and reach new personal records.</p>
+      </div>
+      <div class="flex-none flex flex-col items-center">
+        <img src="bottom-of-squat-600-400.jpg" alt="lifter at the bottom of a squat" class="w-96"/>
+        <cite class="text-right w-full">
+          <span class="text-xs"><a href="https://startingstrength.com/training/back-angle-in-the-squat-part-2-how-to-do-it">Image credit: Starting Strength</a></span>
+        </cite>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="w-full border-opacity-50 text-center py-12 bg-secondary">
+  <div class="container mx-auto px-2 text-secondary-content">
     <h2 class="text-2xl md:text-3xl py-4 font-bold">Coach-in-Training Special</h2>
-    <p >Gus is pursuing the Starting Strengh Coach (SSC) credential. As thanks for being one of his initial clients, all sessions are currently "pay what you want."</p>
+    <p >Join Gus in his early career journey and enjoy our small group training policy for all training sessions. Your early trust doesn't just support Gus; it also ensures you become a valued founding member of a growing strength community.</p>
     <p class="flex flex-col text-center py-6">
       <span class="text-3xl font-bold">$20</span>
-      <span class="text-xs">recommended per 1-hour session</span>
+      <span class="text-xs">suggested contribution</span>
     </p>
   </div>
 </div>
