69d8d8bd431dd35ba2a0c0a1de02f75c5073bb3a
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-07-16 10:13:12 -0400

    a11y: figcaption must be in figure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,10 +52,10 @@
   <div class="container mx-auto px-2">
     <h2 class="text-2xl md:text-3xl py-4 mb-8 font-bold text-center">Coach Alex "Gus" Gustafson</h2>
     <div class="flex flex-col lg:flex-row px-2 lg:px-24 xl:px-48">
-      <div class="flex-none flex flex-col items-center pb-12">
+      <figure class="flex-none flex flex-col items-center pb-12">
         <img src="alex-power-clean.png" alt="Gus racking a power clean." class="w-96" width="1080 " height="572"/>
         <figcaption class="text-left text-xs w-full">Gus racking a power clean.</figcaption>
-      </div>
+      </figure>
       <div class="flex-1 lg:pl-6 prose">
         <ul>
           <li>Aspiring Starting Strength Coach</li>
