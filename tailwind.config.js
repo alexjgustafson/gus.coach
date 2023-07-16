c919ca15e2cddb45de76db7f2d507db6508153dc
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-07-16 10:11:27 -0400

    Add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Varela Round', 'sans-serif'],
+      accent: ['Patrick Hand SC', 'serif'],
+    }
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
