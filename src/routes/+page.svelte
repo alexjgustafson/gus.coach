b2b7cd7f0ebbb0b3783bda4a25bd73f8d74557a1
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-07-16 10:11:37 -0400

    Use accent font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,7 +37,7 @@
 </div>
 <div class="w-full border-opacity-50 text-center py-12 bg-secondary">
   <div class="container mx-auto px-2 lg:px-48 xl:px-80 text-secondary-content">
-    <h2 class="text-2xl md:text-3xl py-4 font-bold">Coach-in-Training Special</h2>
+    <h2 class="text-3xl md:text-5xl py-4 font-bold font-accent">Coach-in-Training Special</h2>
     <div class="prose text-secondary-content">
       <p >Join Gus in his early career journey and enjoy a "pay what you want" policy for personal training sessions. Your early trust doesn't just support Gus; it also ensures you become a valued founding member of a growing strength community.</p>
     </div>
@@ -45,6 +45,7 @@
       <span class="text-3xl font-bold">$30</span>
       <span class="text-xs">suggested contribution per session</span>
     </p>
+    <p class="font-accent text-2xl"><a href="tel:5024723042">Call (502) 472-3042 to Get Started</a></p>
   </div>
 </div>
 <div class="w-full border-opacity-50 py-12">
