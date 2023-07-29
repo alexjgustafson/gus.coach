28565c6064186348c58e003978f02af06679225c
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-07-29 17:48:17 -0400

    Add lifts pages

---
+++ src/routes/press/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+    <title>Learn the Press - Starting Strength at Gus's Barbell Club</title>
+    <meta name="description" content="Learn the Press at Gus's Barbell Club using the Starting Strength method.">
+</svelte:head>
+<div class="hero min-h-screen" style="background-image:url('foggy-gym.jpg');">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content flex-col">
+    <div class="max-w-md">
+      <div class="prose text-neutral-content">
+        <h1 class="text-neutral-content text-5xl">Learn the Press</h1>
+      </div>
+    </div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/8dacy5hjaE8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <div class="hero__photo-credit text-xs absolute bottom-0 right-0">
+      <a href="https://www.pexels.com/photo/close-up-shot-of-a-barbell-6389516/" target="_blank">Photo by Tima Miroshnihcenko</a>
+    </div>
+  </div>
+</div>
