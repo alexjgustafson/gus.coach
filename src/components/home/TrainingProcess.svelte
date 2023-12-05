f6411faa3e1fae1ec83f40d88bb4ae2a41776a0b
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-12-05 14:37:07 -0500

    slim down training process copy

--- src/components/home/TrainingProcess.svelte
+++ src/components/home/TrainingProcess.svelte
@@ -2,14 +2,14 @@
   <div class="container mx-auto prose text-neutral-content">
     <h2 class="text-neutral-content lg:text-4xl">Training Process</h2>
     <h3 class="text-neutral-content">1. Schedule Your First Session</h3>
-    <p>Gus can meet you at a gym, bring his supplies to your garage or driveway, or you can come train at his home. If you're not sure what's right for you, we can figure it out together! Call or text (502) 472-3042 to schedule.</p>
+    <p>Gus can meet you at a Louisville-area gym or even bring his supplies to your garage or driveway. Call or text (502) 472-3042 to schedule.</p>
     <h3 class="text-neutral-content">2. Learn the Lifts</h3>
     <p>Starting Strength trains the <a href="/squat" class="text-accent">Squat</a>, <a href="press" class="text-accent">Press</a>, <a href="/deadlift" class="text-accent">Deadlift</a>, <a href="/bench-press" class="text-accent">Bench Press</a>, and <a href="/power-clean" class="text-accent">Power Clean</a>. Gus will teach you how to safely and effectively perform each of these lifts.</p>
-    <p>No previous experience is needed. We'll find your starting point!</p>
-    <p>Even if you're an experienced lifter, you'll learn how Starting Strength trains the greatest amount of muscle mass over the longest effective range of motion.</p>
     <h3 class="text-neutral-content">3. Train with Gus and On Your Own</h3>
-    <p>You'll be training multiple sessions per week. Schedule more time with Gus as possible, but we'll also teach you how to train on your own between coaching sessions.</p>
+    <p>You'll train multiple sessions per week. When on your own, Gus can form check videos, answer questions, and help you stay on the program.</p>
     <h3 class="text-neutral-content">4. Improved Results Every Week</h3>
-    <p>Everyone starts with the Novice Linear Progression. You'll see the proof that you're getting stronger in your training log every week. Stick with this first simple program long enough, then Gus will coach you through Intermediate and Advanced programs once you're ready.</p>
+    <p>Everyone starts with the Novice Linear Progression. You'll see the proof that you're getting stronger in your training log every week.</p>
+    <h2 class="text-neutral-content">No experience necessary!</h2>
+    <p>No matter where you start, you can learn to train.</p>
   </div>
 </div>
