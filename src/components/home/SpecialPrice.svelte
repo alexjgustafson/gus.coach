ea132658c5f36f5cbeb51009de6daa6d17776d55
Alex J. Gustafson <alexjgustafson@gmail.com>
2023-12-11 15:37:23 -0600

    add new photo to price section

--- src/components/home/SpecialPrice.svelte
+++ src/components/home/SpecialPrice.svelte
@@ -1,14 +1,26 @@
 <div class="w-full border-opacity-50 text-center py-12 bg-secondary">
   <div class="container mx-auto px-6 lg:px-48 xl:px-80 text-secondary-content">
     <h2 class="text-3xl md:text-5xl py-4 font-bold font-accent">Coach-in-Training Special</h2>
-    <div class="prose text-secondary-content">
-      <p>Join Gus in his early career journey and enjoy a "Pay What You Want" policy for all personal training sessions. Your early trust doesn't just support Gus, it also ensures you become a valued founding member of a growing strength community.</p>
+  </div>
+  <div class="flex flex-col lg:flex-row px-2 lg:px-24 xl:px-48">
+    <div class="flex-none flex flex-col items-center">
+      <img src="rip-coaching-gus-deadlift.png" alt="Rip looks approvingly as Gus deadlifts at the Wichita Falls Athletic Club" class="w-96" width="384" height="485" />
+      <cite class="text-right w-full">
+        <span class="text-xs text-secondary-content">Rip coaches Alex during the Starting Strength Seminar.</span>
+      </cite>
     </div>
-    <p class="flex flex-col text-center py-6">
-      <span class="text-3xl font-bold">$60</span>
-      <span class="text-xs">suggested contribution per session</span>
-    </p>
-    <p class=""><span class="font-bold italic text-xl">Free</span> programming and online form checks between sessions.</p>
+    <div class="flex flex-col justify-center flex-1 lg:pl-6 pb-12 prose text-secondary-content">
+      <div class="prose text-secondary-content">
+        <p>Join Gus in his early career journey and enjoy a "Pay What You Want" policy for all personal training sessions. Your early trust doesn't just support Gus, it also ensures you become a valued founding member of a growing strength community.</p>
+      </div>
+      <p class="flex flex-col text-center py-6">
+        <span class="text-3xl font-bold">$60</span>
+        <span class="text-xs">suggested contribution per session</span>
+      </p>
+      <p class=""><span class="font-bold italic text-xl">Free</span> programming and online form checks between sessions.</p>
+    </div>
+  </div>
+  <div class="container mx-auto px-6 lg:px-48 xl:px-80 text-secondary-content">
     <p class="font-accent text-3xl mt-12"><a href="tel:5024723042">Call (502) 472-3042 to Get Started</a></p>
   </div>
 </div>
