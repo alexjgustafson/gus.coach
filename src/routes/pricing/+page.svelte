2d05cbd280af3e9bb9f89f6a1b4dbb8b3141f73e
Alex J. Gustafson <alexjgustafson@gmail.com>
2024-03-22 00:24:36 -0400

    add pricing page

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,59 @@
+<svelte:head>
+    <title>Pricing - Starting Strength at Gus's Barbell Club</title>
+    <meta name="description" content="Pricing to train at Gus's Barbell Club using the Starting Strength method.">
+</svelte:head>
+<div class="hero min-h-screen" style="background-image:url('foggy-gym.jpg');">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content flex-col">
+    <div class="max-w-md">
+      <div class="prose text-neutral-content">
+        <h1 class="text-neutral-content text-5xl">Pricing</h1>
+      </div>
+    </div>
+    <div class="hero__photo-credit text-xs absolute bottom-0 right-0">
+      <a href="https://www.pexels.com/photo/close-up-shot-of-a-barbell-6389516/" target="_blank">Photo by Tima Miroshnihcenko</a>
+    </div>
+  </div>
+</div>
+<div class="w-full border-opacity-50 py-12">
+  <div class="container mx-auto px-2 prose">
+    <h2>Introductory Training Programs</h2>
+    <p>All programs include a one-hour complimentary intro session.</p>
+
+    <h3>Express</h3>
+    <h4>$265 6-week Package</h4>
+    <p class="font-accent">For athletes who want to kickstart their training.</p>
+    <ul>
+      <li>Four personal training sessions available as desired within a six-week span</li>
+      <li>Two remote check-ins per week for video form checks, programming, Q&A</li>
+      <li>Paid as $265 one-time</li>
+    </ul>
+
+    <h3>Effective</h3>
+    <h4>$675 12-week package</h4>
+    <p class="font-accent">For athletes who value personal coaching throughout their novice program.</p>
+    <ul>
+      <li>One personal training session per week for 12 weeks</li>
+      <li>Two remote check-ins per week for video form checks, programming, Q&A</li>
+      <li>Additional sessions during 12 weeks available at a discounted rate of $55</li>
+      <li>Paid as $225 per month over 3 months</li>
+    </ul>
+
+    <h3>Elite</h3>
+    <h4>$795 12- to 16-week package</h4>
+    <p class="font-accent">For athletes who are all-in on beginning their training journey.</p>
+    <ul>
+      <li>One personal training session per week for 12 weeks</li>
+      <li>Two remote check-ins per week for video form checks, programming, Q&A</li>
+      <li>Four additional training sessions anytime within 16 weeks of beginning.</li>
+      <li>Additional sessions during 16 weeks available at a discounted rate of $50</li>
+      <li>Paid as $265 per month over 3 months</li>
+    </ul>
+
+    <h3>Alumni</h3>
+    <p>After completing your introductory program receive discounted pricing by continuing a monthly subscription.</p>
+
+    <h2>Á La Carte</h2>
+    <p>$60 per session. Does not include remote coaching between sessions.</p>
+  </div>
+</div>
