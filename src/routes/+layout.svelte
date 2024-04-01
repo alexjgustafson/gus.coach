d09bb9f6d06938754675de617d31cf5acfe7769d
Alex J. Gustafson <alexjgustafson@gmail.com>
2024-04-01 19:52:10 -0400

    Add Contact Form section to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
   import "../app.css";
+  import ContactForm from '../components/ContactForm.svelte';
   import Footer from '../components/Footer.svelte';
 </script>
 
 <slot />
+<ContactForm />
 <Footer />
