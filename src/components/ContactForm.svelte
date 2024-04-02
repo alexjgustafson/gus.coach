2500eac144e6660a1e8819ea94950f063db2ddc7
Alex J. Gustafson <alexjgustafson@gmail.com>
2024-04-01 20:15:33 -0400

    use netlify's svelte-specific form example

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -1,25 +1,14 @@
 <div class="bg-secondary p-10">
   <div class="container prose text-secondary-content px-6">
-    <form name="contact" method="POST" netlify>
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+    <form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+      <input type="hidden" name="form-name" value="netlify-form-example" />
+      <label for="name">Name</label>
+      <input name="name" id="name" required placeholder="Name" type="text" />
+      <label for="email">Email</label>
+      <input name="email" id="email" required placeholder="Email" type="email" />
+      <label for="message">Message</label>
+      <input name="message" id="message" required placeholder="Message" type="text" />
+      <input type="submit" value="Submit" />
     </form>
-    
   </div>
 </div>
