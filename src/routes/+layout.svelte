20af289dc76577561a7f16b224ac299f33b1fb29
Alex J. Gustafson <alexjgustafson@gmail.com>
2024-04-07 21:49:40 -0400

    Remove the contact form section that stinks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script>
   import "../app.css";
-  import ContactForm from '../components/ContactForm.svelte';
   import Footer from '../components/Footer.svelte';
 </script>
 <slot />
-<ContactForm />
 <Footer />
