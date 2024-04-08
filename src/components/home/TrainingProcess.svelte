6a0518eb7447250ae0a1681b8f5e1c8ba0097bdb
Alex J. Gustafson <alexjgustafson@gmail.com>
2024-04-07 22:49:09 -0400

    Update training process copy

--- src/components/home/TrainingProcess.svelte
+++ src/components/home/TrainingProcess.svelte
@@ -1,12 +1,16 @@
 <div class="w-full border-opacity-50 py-12 px-6 lg:px-48 bg-neutral">
   <div class="container mx-auto prose text-neutral-content">
     <h2 class="text-neutral-content lg:text-4xl">Training Process</h2>
-    <h3 class="text-neutral-content">1. Schedule Your Intro Session</h3>
-    <p>Call or text (502) 472-3042 to schedule a free session. You will learn the squat and create a training plan with Gus. The barbell club's new location is in the works, in the meantime other arrangements can be determined: even your home garage or driveway.</p>
+    <h3 class="text-neutral-content">1. Pick Your Intro Session: Low Sweat or No Sweat</h3>
+    <p>Call or text (502) 472-3042 to schedule a free session. You can get moving right away or we can start with a chat.</p>
+    <h4 class="text-xl italic text-neutral-content pl-8">Low Sweat:</h4>
+    <p class="pl-8">In a free one-hour session you will learn the squat, get a feel for what it's like to work with Gus, and discuss an initial training plan.</p>
+    <h4 class="text-xl italic text-neutral-content pl-8">No Sweat:</h4>
+    <p class="pl-8">Come see the gym, shake hands and chat with me for 15 minutes. You won't be asked to squeeze in a workout or make any commitments.</p>
     <h3 class="text-neutral-content">2. Learn the Lifts</h3>
     <p>Starting Strength trains the <a href="/squat" class="text-accent">Squat</a>, <a href="press" class="text-accent">Press</a>, <a href="/deadlift" class="text-accent">Deadlift</a>, <a href="/bench-press" class="text-accent">Bench Press</a>, and <a href="/power-clean" class="text-accent">Power Clean</a>. Gus will teach you how to safely and effectively perform each of these lifts.</p>
     <h3 class="text-neutral-content">3. Train with Gus and On Your Own</h3>
-    <p>You'll train multiple sessions per week. When on your own, Gus can form check videos, answer questions, and help you stay on the program.</p>
+    <p>You'll train multiple sessions per week. Schedule a session at the barbell club when you can! When on your own, Gus can form check videos, answer questions, and help you stay on the program.</p>
     <h3 class="text-neutral-content">4. Improved Results Every Week</h3>
     <p>Everyone starts with the Novice Linear Progression. You'll see the proof that you're getting stronger in your training log every week.</p>
     <h2 class="text-neutral-content">No experience necessary!</h2>
